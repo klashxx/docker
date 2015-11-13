b7c0926f5d1f02bb4ce9f49fc95dd8ef364bac4b
klashxx <klashxx@gmail.com>
2015-11-13 01:21:16 +0100

    2 steps design

--- oracle12c/build.sh
+++ oracle12c/build.sh
@@ -1,1 +1,10 @@
-docker build --tag="fedora/oracle12c" .
+docker build --tag="fedora/ora12c_soft" -f Dockerfile.1 .
+
+docker run --privileged=true \
+           -it --cidfile="cidfile" \
+           fedora/ora12c_soft  \
+           /bin/bash -c  "/tmp/privileged_actions.sh"
+
+docker commit $(cat cidfile) fedora/ora12c_soft
+
+#docker build --tag="fedora/ora12c" -f Dockerfile.2 .
