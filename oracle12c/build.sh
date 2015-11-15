bc476793cf99d20558ee7619ac153811c793b54e
klashxx <klashxx@gmail.com>
2015-11-15 22:01:54 +0100

    Refactoring build script

--- oracle12c/build.sh
+++ oracle12c/build.sh
@@ -1,12 +1,24 @@
-docker build --tag="fedora/ora12c_soft" -f Dockerfile.1 .
+#!/bin/bash
+
+readonly SW_IMG="fedora/ora12c_sw"
+readonly DB_IMG="fedora/ora12c_db"
+readonly DB_DIR="database"
+
+[[ ! -d $DB_DIR ]] && echo "Database software required! " && exit 5
+
+docker build --tag=$SW_IMG -f Dockerfile.1 .
 
 docker run --privileged=true \
            -it --cidfile="cidfile.$$" \
-           fedora/ora12c_soft  \
+           $SW_IMG  \
            /bin/bash -c  "/tmp/privileged_actions.sh"
 
-docker commit $(cat cidfile.$$) fedora/ora12c_soft
+docker commit $(cat cidfile.$$) $SW_IMG
 
 rm cidfile.$$
 
-docker build --tag="fedora/ora12c" -f Dockerfile.2 .
+[[ -d $DB_DIR ]] && mv $DB_DIR ${DB_DIR}_bk
+
+docker build --tag=$DB_IMG -f Dockerfile.2 .
+
+[[ -d ${DB_DIR}_bk ]] && mv ${DB_DIR}_bk $DB_DIR
